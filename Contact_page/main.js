4cca842e8f4d666adb653f80fbefc6f8e18f90cd
nclsbayona <nbayonaord@gmail.com>
2022-02-11 17:15:17 -0500

    comentario

--- Contact_page/main.js
+++ Contact_page/main.js
@@ -1,4 +1,5 @@
 function show_alert(){
+    // Funcion para mostrar una alerta en la pagina con los datos del usuario
     let name=document.getElementById("nombre").value;
     let email=document.getElementById("email").value;
     let tel=document.getElementById("telefono").value;
