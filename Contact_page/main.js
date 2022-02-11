d14d80df0e98eeb996a940705bb830910378816b
nclsbayona <nbayonaord@gmail.com>
2022-02-11 17:11:02 -0500

    Almost there...

---
+++ Contact_page/main.js
@@ -0,0 +1,8 @@
+function show_alert(){
+    let name=document.getElementById("nombre").value;
+    let email=document.getElementById("email").value;
+    let tel=document.getElementById("telefono").value;
+    let asun=document.getElementById("asunto").value;
+    let msg=document.getElementById("mensaje").value;
+    alert("Apreciado "+name+"\n"+"Email: "+email+"\n"+"Telefono: "+tel+"\nSe ha escrito un mensaje con asunto "+asun+" y contenido:\n"+msg);
+}
