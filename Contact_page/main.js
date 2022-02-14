a1aa5a079dca37eabc09dd0b1ec0a5e2f00d660d
nclsbayona <nbayonaord@gmail.com>
2022-02-14 14:11:55 -0500

    linting

--- Contact_page/main.js
+++ Contact_page/main.js
@@ -1,9 +1,9 @@
-function show_alert(){
+function show_alert() {
     // Funcion para mostrar una alerta en la pagina con los datos del usuario
-    let name=document.getElementById("nombre").value;
-    let email=document.getElementById("email").value;
-    let tel=document.getElementById("telefono").value;
-    let asun=document.getElementById("asunto").value;
-    let msg=document.getElementById("mensaje").value;
-    alert("Apreciado "+name+"\n"+"Email: "+email+"\n"+"Telefono: "+tel+"\nSe ha escrito un mensaje con asunto "+asun+" y contenido:\n"+msg);
+    let name = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let tel = document.getElementById("telefono").value;
+    let asun = document.getElementById("asunto").value;
+    let msg = document.getElementById("mensaje").value;
+    alert("Apreciado " + name + "\n" + "Email: " + email + "\n" + "Telefono: " + tel + "\nSe ha escrito un mensaje con asunto " + asun + " y contenido:\n" + msg);
 }
